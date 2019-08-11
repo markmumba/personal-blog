924da4d3fc09e4fd79235e3394cec3adfda907a0
markmumba <markmumba01@gmail.com>
2019-08-11 14:56:22 +0300

    setting up my authentacation adding th routes

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,1 +1,17 @@
-from flask import 
+# in the views.py we create the diffrent routes to take on to the rendered html 
+# for the login and to register 
+
+from flask import render_template,redirect,url_for,request,flash
+from .import auth
+from ..models import User
+from flask_login import login_user,logout_user,login_required
+from .forms import RegistrationForm, LoginForm
+from .. import db
+
+
+# the registration route 
+
+@auth.route('/register',methods = ['GET','POST'])
+def register()
+
+
