cb7e01f7f2a2d0a40686afb9626c94f49aa9adc4
markmumba <markmumba01@gmail.com>
2019-08-11 15:16:51 +0300

    setting up my authentacation adding th routes

--- app/auth/views.py
+++ app/auth/views.py
@@ -12,6 +12,46 @@ from .. import db
 # the registration route 
 
 @auth.route('/register',methods = ['GET','POST'])
-def register()
+def register():
+	form = RegistrationForm()
+	if form.validate_on_submit():
+        user =User(email=form.email.data,username=form.username.data,password=form.password.data)
+        db.session.add(user)
+        db.session.commit()
+
+        return redirect(url_for('auth.login'))
+        title='Register your Account'
+    return render_template('auth/register.html',form=form)
 
 
+#  this the login route and validations
+# has also the admin route if is the admin
+
+@auth.route('/login',methods=['GET','POST'])
+def login():
+    '''
+    Function that checks if the form is validated
+    '''
+    login_form=LoginForm()
+    if login_form.validate_on_submit():
+        user=User.query.filter_by(email=login_form.email.data).first()
+        if user is not None and user.verify_password(login_form.password.data):
+            login_user(user,login_form.remember.data)
+
+            # redirect the admin to the admin dashboard
+            if user.is_admin:
+                return redirect(url_for('main.admin_dashboard'))
+            else:
+                return redirect(url_for('./AdminDashboard'))
+        else:
+            flash('Invalid email or password,')
+
+    title ="Brownie's Blog"
+    return render_template('auth/login.html',login_form=login_form,title=title)
+
+# Logout
+@auth.route('/logout')
+@login_required
+def logout():
+	logout_user()
+	return redirect(url_for('main.index'))
