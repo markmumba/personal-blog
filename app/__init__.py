9778265a15b53352d41834d03f45fe4996489d4b
markmumba <markmumba01@gmail.com>
2019-08-12 11:55:05 +0300

    wrote my readme allthough app not working

--- app/__init__.py
+++ app/__init__.py
@@ -14,6 +14,8 @@ login_manager.login_view ='auth.login'
 
 bootstrap = Bootstrap()
 db = SQLAlchemy()
+photos = UploadSet('photos',IMAGES)
+mail = Mail()
 simple = SimpleMDE()
 
 def create_app(config_name):
@@ -28,13 +30,14 @@ def create_app(config_name):
     bootstrap.init_app(app)
     db.init_app(app)
     login_manager.init_app(app)
+    mail.init_app(app)
     simple.init_app(app)
 
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
 
     from .auth import auth as auth_blueprint
-    app.register_blueprint(auth_blueprint,url_prefix = '/authenticate')
+    app.register_blueprint(auth_blueprint,url_prefix = '/auth')
 
     from .admin import admin as admin_blueprint
     app.register_blueprint(admin_blueprint,url_prefix = '/admin')
