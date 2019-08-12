2b5d4089bcf7ed012cab5488d7f08fbbdccf0b57
markmumba <markmumba01@gmail.com>
2019-08-12 12:31:02 +0300

    trying to launch app has a lot of bugs

--- app/auth/views.py
+++ app/auth/views.py
@@ -2,31 +2,28 @@
 # for the login and to register 
 
 from flask import render_template,redirect,url_for,request,flash
-from .import auth
+from . import auth
 from ..models import User
 from flask_login import login_user,logout_user,login_required
 from .forms import RegistrationForm, LoginForm
 from .. import db
 
-
-# the registration route 
-
-@auth.route('/register',methods = ['GET','POST'])
+# registration route
+@auth.route('/reqister',methods=['GET','POST'])
 def register():
-	form = RegistrationForm()
-	if form.validate_on_submit():
+    form =RegistrationForm()
+    if form.validate_on_submit():
         user =User(email=form.email.data,username=form.username.data,password=form.password.data)
         db.session.add(user)
         db.session.commit()
 
         return redirect(url_for('auth.login'))
         title='Register your Account'
-    return render_template('auth/register.html',form=form)
+    return render_template('auth/register.html',registration_form=form)
 
 
-#  this the login route and validations
-# has also the admin route if is the admin
 
+# Login
 @auth.route('/login',methods=['GET','POST'])
 def login():
     '''
@@ -49,9 +46,60 @@ def login():
     title ="Brownie's Blog"
     return render_template('auth/login.html',login_form=login_form,title=title)
 
-# Logout
+#logout
 @auth.route('/logout')
 @login_required
 def logout():
-	logout_user()
-	return redirect(url_for('main.index'))
+    logout_user()
+    return redirect(url_for('main.index'))
+from flask import render_template,redirect,url_for,request,flash
+from . import auth
+from ..models import User
+from flask_login import login_user,logout_user,login_required
+from .forms import RegistrationForm, LoginForm
+from .. import db
+
+# registration route
+@auth.route('/reqister',methods=['GET','POST'])
+def register():
+    form =RegistrationForm()
+    if form.validate_on_submit():
+        user =User(email=form.email.data,username=form.username.data,password=form.password.data)
+        db.session.add(user)
+        db.session.commit()
+
+        return redirect(url_for('auth.login'))
+        title='Register your Account'
+    return render_template('auth/register.html',registration_form=form)
+
+
+
+# Login
+@auth.route('/login',methods=['GET','POST'])
+def login():
+    '''
+    Function that checks if the form is validated
+    '''
+    login_form=LoginForm()
+    if login_form.validate_on_submit():
+        user=User.query.filter_by(email=login_form.email.data).first()
+        if user is not None and user.verify_password(login_form.password.data):
+            login_user(user,login_form.remember.data)
+
+            # redirect the admin to the admin dashboard
+            if user.is_admin:
+                return redirect(url_for('main.admin_dashboard'))
+            else:
+                return redirect(url_for('./AdminDashboard'))
+        else:
+            flash('Invalid email or password,')
+
+    title ="Brownie's Blog"
+    return render_template('auth/login.html',login_form=login_form,title=title)
+
+#logout
+@auth.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('main.index'))
