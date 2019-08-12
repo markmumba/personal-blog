52f60486a86e348c6f1ba0aad6b5fa39d8abd0db
markmumba <markmumba01@gmail.com>
2019-08-12 21:32:41 +0300

    making some changes

--- app/models.py
+++ app/models.py
@@ -9,88 +9,67 @@ def load_user(user_id):
     return User.query.get(int(user_id))
 
 class User(UserMixin,db.Model):
-    __tablename__='users'
-
-    id=db.Column(db.Integer,primary_key = True)
-    username = db.Column(db.String(255),index = True)
-    email = db.Column(db.String(255),unique=True,index=True)
-    password_hash = db.Column(db.String())
-    role_id = db.Column(db.Integer,db.ForeignKey('roles.id'))
+    __tablename__ = 'users'
+    id = db.Column(db.Integer,primary_key = True)
+    username = db.Column(db.String(255))
+    email = db.Column(db.String(255),unique = True,index = True)
+    bio = db.Column(db.String(255))
+    profile_pic_path = db.Column(db.String())
+    pass_secure = db.Column(db.String(255))
+    blogs = db.relationship('Blog',backref = 'user',lazy = "dynamic")
 
     @property
     def password(self):
-        raise AttributeError("You cannot read the password attribute")
+        raise AttributeError('You cannot read the password attribute')
 
     @password.setter
-    def password(self,password):
-        self.password_hash = generate_password_hash(password)
+    def password(self, password):
+        self.pass_secure = generate_password_hash(password)
+
 
     def verify_password(self,password):
-        return check_password_hash(self.password_hash,password)
-
-    def save_user(self):
-        db.session.add(self)
-        db.session.commit()
+        return check_password_hash(self.pass_secure,password)
 
     def __repr__(self):
-        return f'User{self.username}'
+        return f'User {self.username}'
 
-class Subscriber(db.Model):
-    __tablename__='subscribers'
+class Blog(db.Model):
 
-    id=db.Column(db.Integer,primary_key=True)
-    email = db.Column(db.String(255),unique=True,index=True)
+    __tablename__ = 'blogs'
 
-    def save_subscriber(self):
-        db.session.add(self)
-        db.session.commit()
-
-
-class Post(db.Model):
-    __tablename__ = 'posts'
-
-    id = db.Column(db.Integer,primary_key=True)
-    post_id=db.Column(db.Integer)
+    id = db.Column(db.Integer,primary_key = True)
+    # upvotes = db.Column(db.Integer)
+    # downvotes = db.Column(db.Integer)
     title = db.Column(db.String)
-    post = db.Column(db.String)
-    category = db.Column(db.String)
-    like=db.Column(db.Integer)
+    content = db.Column(db.String)
     posted = db.Column(db.DateTime,default=datetime.utcnow)
+    user_id = db.Column(db.Integer,db.ForeignKey("users.id"))
+    comments = db.relationship('Comment',backref = 'blogs',lazy = "dynamic")
 
-
-    def save_post(self):
+    def save_blog(self):
         db.session.add(self)
         db.session.commit()
 
     @classmethod
-    def get_posts(cls,id):
-        posts=Post.query.filter_by(post_id=id).all()
-        return posts
+    def get_blogs(cls):
+        blogs = Blog.query.order_by(Blog.posted.desc()).all()
+        return blogs
 
 class Comment(db.Model):
-    __tablename__='comments'
+
+    __tablename__ = 'comments'
 
     id = db.Column(db.Integer,primary_key = True)
-    comment = db.Column(db.String)
+    content = db.Column(db.String)
     posted = db.Column(db.DateTime,default=datetime.utcnow)
-    post_id = db.Column(db.Integer,db.ForeignKey("posts.id"))
-    user_id = db.Column(db.Integer,db.ForeignKey("users.id"))
+    blog_id = db.Column(db.Integer,db.ForeignKey("blogs.id"))
 
     def save_comment(self):
         db.session.add(self)
         db.session.commit()
 
-    def delete_comment(self):
-        db.session.delete(self)
-        db.session.commit()
-
-
-class Role(db.Model):
-    __tablename__ = 'roles'
-
-    id = db.Column(db.Integer,primary_key=True)
-    name = db.Column(db.String(255))
-    users= db.relationship('User',backref='role',lazy="dynamic")
-
-    def __repr__(self):
-        return f'User{self.name}'
+    @classmethod
+    def get_comments(cls):
+        comments = Comment.query.order_by(Comment.posted.desc()).all()
+        return comments
+    
