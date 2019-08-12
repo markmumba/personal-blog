57a954760cfa96a103596c7b753e144acbbc8114
markmumba <markmumba01@gmail.com>
2019-08-12 12:18:21 +0300

    trying to launch app has a lot of bugs

--- manage.py
+++ manage.py
@@ -1,25 +1,29 @@
-from app import create_app,db
-from flask_script import Manager,Server
-from app.models import Blog,User,Comments
-from  flask_migrate import Migrate, MigrateCommand
-from flask_login import LoginManager
+from app import create_app, db
+from flask_script import Manager, Shell, Server
+from app.models import User
+from flask_migrate import Migrate, MigrateCommand
 
+# Creating app instance
 
+app = create_app('development')
 
-# Creating app instances
-# app = create_app('development')
-app = create_app('production')
 
 manager = Manager(app)
-manager.add_command('server',Server)
-
-#Migration
 migrate = Migrate(app,db)
+manager.add_command('server', Server)
 manager.add_command('db',MigrateCommand)
 
+
+@manager.command
+def test():
+    """Run the unit tests."""
+    import unittest
+    tests = unittest.TestLoader().discover('tests')
+    unittest.TextTestRunner(verbosity=2).run(tests)
+    
 @manager.shell
 def make_shell_context():
-    return dict(app = app, db = db,Blog = Blog, User = User, Comments = Comments)
+    return dict(app = app,db = db,User = User)
 
 
 if __name__ == '__main__':
